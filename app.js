ec21fb55ec952427e8243623423be6302405d403
sks147 <sksingh1997@gmail.com>
2019-06-11 08:21:38 +0530

    Socket.io connection working

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const express = require('express'),
   mongoose = require('mongoose'),
   path = require('path'),
   bodyParser = require('body-parser'),
-  cors = require('cors')
+  cors = require('cors'),
+  http = require('http'),
+  socketio = require('socket.io')
 
 const dbURI = `${process.env.MONGODB_URI}`
 
 const signin = require('./routes/signin')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', () => {
+  console.log('Websocket connection working')
+})
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useFindAndModify: false })
 const db = mongoose.connection
@@ -29,6 +37,6 @@ app.set('port', process.env.PORT || 3001)
 
 app.use(express.static(path.resolve(__dirname, './fe/build')))
 
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
   console.log('Node app is running at localhost:' + app.get('port'))
 })
